f1b1af8e8daae74d0bbcf6891ec9715dee2f546b
Manya7864 <manishyadav78642080@gmail.com>
2022-05-31 18:24:32 +0530

    form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import AS from "axios"
+import Form from '../src/Component/Form'
+import React, { useState, useEffect } from 'react';
+import Table from '../src/Component/Table'
+import Top from '../src/Component/Top'
 
 function App() {
+  const [data, setData] = useState({});
+  const [userData, setUserData] = useState([]);
+
+  useEffect(() => {
+    AS
+      .get(`http://localhost:8080/data`)
+      .then(r => {
+        setUserData(r.data);
+        //console.log(r);
+      })
+  }, [setData])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Top></Top>
+      <div className="container">
+        <div>
+          <Form data={data} setData={setData} setUserData={setUserData} userData={userData}></Form>
+        </div>
+        <div>
+          <Table userData={userData}></Table>
+        </div>
+      </div>
+    </>
   );
 }
 
